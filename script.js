14d2e5ce3b22f13926c8a64aae2e36f6875d4757
Bret Zawilski <bret.zawilski@gmail.com>
2021-04-25 22:46:11 +0100

    Inactive Switch/Case attempt

--- script.js
+++ script.js
@@ -9,22 +9,86 @@ function computerPlay() {
   let computerChoice = getRandomIntInclusive(1, 3);
 
   if (computerChoice === 1) {
-    return "Rock";
+    return "rock";
   } else if (computerChoice === 2) {
-    return "Paper";
+    return "paper";
   } else {
-    return "Scissors";
+    return "scissors";
   }
 
 }
 
 function playerChoice(playerParam) {
   let selection = playerParam.toLowerCase()
-  if (selection != "rock" && selection != "paper" && selection != "scissors") {
+  if (selection != "rock" && selection != "paper" && selection != "scissors" && selection != undefined && selection != null) {
     console.log("Invalid choice, please try again.")
   } else {
     return selection;
   }
 }
 
-computerSelection = (computerPlay());
+let playerSelection = playerChoice(prompt('Rock, Paper, or Scissors?'));
+let computerSelection = (computerPlay());
+
+if (playerSelection === "rock" && computerSelection === "rock"){
+  console.log("tie");
+}
+else if (playerSelection === "rock" && computerSelection === "scissors") {
+  console.log("player wins");
+}
+else if (playerSelection === "rock" && computerSelection === "paper") {
+  
+}
+else if (playerSelection === "scissors" && computerSelection === "scissors") {
+  console.log("tie");
+}
+else if (playerSelection === "scissors" && computerSelection === "paper") {
+  console.log("player wins");
+}
+else if (playerSelection === "scissors" && computerSelection === "rock") {
+  
+}
+else if (playerSelection === "paper" && computerSelection === "paper") {
+  console.log("tie");
+}
+else if (playerSelection === "paper" && computerSelection === "rock") {
+  console.log("player wins");
+}
+else if (playerSelection === "paper" && computerSelection === "scissors") {
+  console.log("computer wins");
+}
+
+
+
+
+// switch (playerSelection, computerSelection) {
+//   case playerSelection === "rock" && computerSelection === "rock":
+//     console.log("tie");
+//     break;
+//   case playerSelection === "rock" && computerSelection === "scissors":
+//     console.log("player wins");
+//     break;
+//   case playerSelection === "rock" && computerSelection === "paper":
+//     console.log("computer wins");
+//     break;
+//   case playerSelection === "scissors" && computerSelection === "scissors":
+//     console.log("tie");
+//     break;
+//   case playerSelection === "scissors" && computerSelection === "paper":
+//     console.log("player wins");
+//     break;
+//   case playerSelection === "scissors" && computerSelection === "rock":
+//     console.log("computer wins");
+//     break;
+//   case playerSelection === "paper" && computerSelection === "paper":
+//     console.log("tie");
+//     break;
+//   case playerSelection === "paper" && computerSelection === "rock":
+//     console.log("player wins");
+//     break;
+//   case playerSelection === "paper" && computerSelection === "scissors":
+//     console.log("computer wins");
+//     break;
+// }
+console.log(playerSelection);
+console.log(computerSelection);
