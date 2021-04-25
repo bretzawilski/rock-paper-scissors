dee0ae367b8d0bce5ad20eebabaf7567f22c8b67
Bret Zawilski <bret.zawilski@gmail.com>
2021-04-25 22:03:35 +0100

    Set up ComputerPlay function

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log("Testing");
+
+function computerPlay(){
+  
+  function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min +1) + min);
+  }
+
+  let computerChoice = getRandomIntInclusive(1, 3);
+
+  if (computerChoice === 1) {
+    return "Rock";
+  } else if (computerChoice ===2) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+
+}
+
+console.log(computerPlay());
