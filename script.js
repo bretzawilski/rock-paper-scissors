6f080559e090deebe11dffd92a7d20b5c433eacb
Bret Zawilski <bret.zawilski@gmail.com>
2021-04-26 10:59:22 +0100

    Refactored game loop; started separating functions

--- script.js
+++ script.js
@@ -2,49 +2,16 @@ function game() {
 
   let computerScore = 0;
   let playerScore = 0;
-  let score = 0;
 
   while (playerScore < 3 && computerScore < 3) {
-    function computerPlay() {
-      
-      function getRandomIntInclusive(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min +1) + min);
-      }
-
-      let computerChoice = getRandomIntInclusive(1, 3);
-
-      if (computerChoice === 1) {
-        return "rock";
-      } else if (computerChoice === 2) {
-        return "paper";
-      } else {
-        return "scissors";
-      }
-
-    }
-
-    function playerChoice(playerParam) {
-      if (playerParam === undefined || playerParam === "" || playerParam === null){
-        alert("Invalid choice. Please try again.")
-        playerChoice(prompt('Rock, Paper, or Scissors?'));
-      }
-      let selection = playerParam.toLowerCase()
-      if (selection != "rock" && selection != "paper" && selection != "scissors") {
-        alert("Invalid choice. Please try again.")
-        playerChoice(prompt('Rock, Paper, or Scissors?'));
-      } else {
-        return selection;
-      }
-    }
-
+    
     let playerSelection = playerChoice(prompt('Rock, Paper, or Scissors?'));
     let computerSelection = (computerPlay());
 
     console.log(`The Player chose ${playerSelection}, and the Computer chose ${computerSelection}`)
 
-
+    // If statement assesses input of player and computer; adds to score when result is not a tie
+    // ! This seems overworked. Is there a way to simplify?
     if (playerSelection === "rock" && computerSelection === "rock"){
       console.log("tie");
     }
@@ -78,17 +45,67 @@ function game() {
       console.log("computer wins");
       computerScore++
     }
-    console.log(`The player's score is ${playerScore}`);
-    console.log(`The computer's score is ${computerScore}`);
+    
+    displayScore(playerScore, computerScore);
 
-    if (playerScore === 3) {
-      console.log("The player wins the game!");
-    }
-    else if (computerScore === 3) {
-      console.log("The computer wins the game!");
-    }
+  }
+
+  winCondition(playerScore, computerScore);
+}
+
+// FUNCTIONS BELOW THIS POINT
+
+const winCondition = function(player, computer) {
+  
+  if (player === 3) {
+    alert("The player wins the game!");
+  }
+  else if (computer === 3) {
+    alert("The computer wins the game!");
   }
 }
 
+const playerChoice = function(playerParam) {
+  
+  if (playerParam === undefined || playerParam === "" || playerParam === null){
+    alert("Invalid choice. Please try again.")
+    playerChoice(prompt('Rock, Paper, or Scissors?'));
+  } 
+
+  let selection = playerParam.toLowerCase()
+  if (selection != "rock" && selection != "paper" && selection != "scissors") {
+    alert("Invalid choice. Please try again.")
+    playerChoice(prompt('Rock, Paper, or Scissors?'));
+  } else {
+    return selection;
+  }
+}
+
+const computerPlay = function() {
+      
+  const computerChoice = getRandomIntInclusive(1, 3);
+
+  if (computerChoice === 1) {
+    return "rock";
+  } else if (computerChoice === 2) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+const getRandomIntInclusive = function(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min +1) + min);
+}
+
+
+const displayScore = function(player, computer) {    
+    console.log(`The player's score is ${player}`);
+    console.log(`The computer's score is ${computer}`);
+}
+
+// Defines start-button on index.html as a button to begin the game function.
 let startGame = document.getElementById('start-button');
 startGame.addEventListener('click', game);
